f4b50fd31815b4224a5b3cda54cc7b3697d109f0
Kattrina <kattrina.erillo@gmail.com>
2021-02-09 20:32:19 -0500

    added custom color

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -1,13 +1,20 @@
+
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          '100': '#D4EFCB'
+        },
+      },
+    },
   },
   variants: {
+    outline: ["focus"],
     extend: {},
   },
   plugins: [],
 }
-
