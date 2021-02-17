717203fa694befaedecb38f264e4ab2ff8182da5
katterillo <kattrina.erillo@gmail.com>
2021-02-16 21:45:16 -0500

    added custom font Open Sans

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -5,6 +5,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        custom: ['Open Sans', 'sans-serif'],
+      },
       colors: {
         green: {
           '100': '#D4EFCB'
