d30c61783e2ee62c9fb6045c5a55fbc7aa19b2c2
katterillo <kattrina.erillo@gmail.com>
2021-03-01 10:44:05 -0500

    formatting

--- app/controllers/concerns/Spoonacular.rb
+++ app/controllers/concerns/Spoonacular.rb
@@ -3,9 +3,7 @@ class Spoonacular
         resp = Faraday.get('https://api.spoonacular.com/recipes/complexSearch') do |req|
             req.params['apiKey'] = ENV["SPOONACULAR_API_KEY"]
             req.params['includeIngredients'] = query
-           #req.params['addRecipeInformation'] = true
             req.params['fillIngredients'] = true
-
           end
           return JSON.parse(resp.body)["results"]
     end
