fad311f680f78b5ff338c39c564518a579d4e021
katterillo <kattrina.erillo@gmail.com>
2021-03-01 00:18:02 -0500

    styled home page

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -5,16 +5,18 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      fontFamily: {
-        custom: ['Open Sans', 'sans-serif'],
-      },
       colors: {
-        green: {
-          '100': '#D4EFCB'
-        },
+          'gray-dark': '#e2e5ec',
+          'gray-medium':'#f4f5f9',
+          'gray-light': '#f4f5f9',
+        }
+     /* fontFamily: {
+        custom: ['Open Sans', 'sans-serif'],
+      },*/
+        
+        }
       },
-    },
-  },
+
   variants: {
     outline: ["focus"],
     extend: {},
