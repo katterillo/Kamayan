33c58e16e87e9aabd1f7e0576975df49b1da4846
katterillo <kattrina.erillo@gmail.com>
2021-03-02 03:08:48 -0500

    added color

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -10,6 +10,7 @@ module.exports = {
           'gray-medium':'#f4f5f9',
           'gray-light': '#f4f5f9',
           'green-dark': '#004e36',
+          'beige': '#f8f7f5',
         },
         fontFamily: {
           custom: ['Montserrat', 'sans-serif']
