7e1bee9cf4fbd7276e9f95829c9cef69e4c0aff5
katterillo <kattrina.erillo@gmail.com>
2021-03-09 10:37:12 -0500

    added nested class attributes to form and model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,6 @@ class Recipe < ApplicationRecord
     has_many :ingredients, dependent: :destroy
     has_many :steps, dependent: :destroy
 
-    accepts_nested_attributes_for :ingredient, reject_if: :all_blank, allow_destroy: true
+    accepts_nested_attributes_for :ingredients, allow_destroy: true
   end
   
