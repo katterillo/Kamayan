3dcfe4037965a6b53dd08d21c458ee4a9b5fd0d5
Wilbert De La Cruz <wilbert@bytraject.com>
2021-03-17 10:04:56 -0400

    move add field js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,17 +13,3 @@ require("jquery")
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-$("[data-form-prepend]").on("click", function(e) {
-  console.log("here");
-    var obj = $($(this).attr("data-form-prepend"));
-    obj.find("input, select, textarea").each(function() {
-      $(this).attr("name", function() {
-        return $(this)
-          .attr("name")
-          .replace("new_record", new Date().getTime());
-      });
-    });
-    obj.insertBefore(this);
-    return false;
-  });
