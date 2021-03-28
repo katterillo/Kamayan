ecb07c730daf8da8fff9b493a2f867258141f856
katterillo <kattrina.erillo@gmail.com>
2021-03-28 15:41:46 -0400

    WIP displaying recipes that belong to a user

---
+++ app/controllers/my_recipes_controller.rb
@@ -0,0 +1,11 @@
+class MyRecipesController < ApplicationController
+    before_action :authenticate_user!
+
+    def index 
+       @recipe = Recipe.all
+    end
+
+    def show
+        @recipe = Recipe.find(params[:id])
+    end
+end
