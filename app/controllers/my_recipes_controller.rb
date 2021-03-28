ea25d0e2c370ee25f81235a3e645eeea508bce3f
katterillo <kattrina.erillo@gmail.com>
2021-03-28 17:36:24 -0400

    WIP directing to recipes

--- app/controllers/my_recipes_controller.rb
+++ app/controllers/my_recipes_controller.rb
@@ -2,10 +2,14 @@ class MyRecipesController < ApplicationController
     before_action :authenticate_user!
 
     def index 
-       @recipe = Recipe.all
-    end
+        @my_recipes = current_user.recipes
+        respond_to do |format|
+         format.html { render :index, locals: { recipes: @recipes } }
+       end
+     end
 
     def show
         @recipe = Recipe.find(params[:id])
     end
+
 end
